2adc19a50a5e0c2c82da098a29035123e4d5bfae
Daniel Ferreira <danieltf88@gmail.com>
2017-11-29 23:33:52 +0000

    some comments

--- word_builder.py
+++ word_builder.py
@@ -2,26 +2,39 @@
 
 class WordBuilder():
     def build(self, word, word_pieces):
+
+
+        ## just to count the number of letters in words pieces,
+        ## and raise exception is there is not enough letter to build
+        ## the word.
         all_letters_in_word_pieces = ''
         for piece in word_pieces:
             all_letters_in_word_pieces += piece
         if len(all_letters_in_word_pieces) < len(word):
             raise RuntimeError("Not enough letters in word pieces!")
 
+
+        ## if there is only one piece, return that piece.
+        ## It should match the word
         if len(word_pieces) == 1:
             return [0]
+
+
+        ## code that does something is here,
+        ## only works with pieces of one or two letters
         idx_list = []
         letters = list(word)
-        jump = False
+        skip_next_letter = False
+
         for idx, letter in enumerate(letters):
-            if jump == True:
-                jump = False
+            if skip_next_letter == True:
+                skip_next_letter = False
                 continue
             try:
                 two_letters = letter + letters[idx+1]
                 if two_letters in word_pieces:
                     idx_list.append(word_pieces.index(two_letters))
-                    jump = True
+                    skip_next_letter = True
                 elif letter in word_pieces:
                     idx_list.append(word_pieces.index(letter))
             except IndexError:
