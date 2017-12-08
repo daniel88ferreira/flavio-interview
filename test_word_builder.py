4e823aa5ef8c2cade032c1743838d4f8375eb2e7
Daniel Ferreira <danieltf88@gmail.com>
2017-12-08 02:33:55 +0000

    added support for test_word_build_long testcase.

--- test_word_builder.py
+++ test_word_builder.py
@@ -20,15 +20,47 @@ class TestWordBuilder(unittest.TestCase):
 
     def test_word_build_long(self):
         word = "palavra"
-        word_pieces = [ 'pa', 'p', 'al', 'av', 'ra', 'la', 'vr']
+        word_pieces = ['pa', 'p', 'al', 'av', 'ra', 'la', 'vr']
         expected = [1,2,3,4]
-        self.assertEqual(self.wb.build(word=word, word_pieces=word_pieces), expected)
+        self.assertEqual(expected, self.wb.build(word=word, word_pieces=word_pieces))
+
+    def test_word_build_impossible(self):
+        word = "palavra"
+        word_pieces = ['ba', 't', 'at', 'oz', 'ia']
+        expected = []
+        self.assertEqual(expected, self.wb.build(word=word, word_pieces=word_pieces))
+
 
     def test_word_build_with_missing_pieces(self):
         word = "palavra"
         word_pieces = ['pa', 'la', 'v', 'a']
         self.assertRaises(RuntimeError,
                           lambda: self.wb.build(word=word, word_pieces=word_pieces))
+
+    def test_get_pieces_in_order(self):
+        word = "pai"
+        word_pieces = ['i', 'pa']
+        expected = ['pa', 'i']
+        self.assertEqual(expected, self.wb.get_pieces_in_order(word_pieces, word))
+
+    def test_word_pieces_match_word(self):
+        word = "pai"
+        word_pieces = ['pa', 'i']
+        self.assertEqual(True, self.wb.word_pieces_match_word(word_pieces, word))
+
+        word_pieces = ['pato', 'i']
+        self.assertEqual(False, self.wb.word_pieces_match_word(word_pieces, word))
+
+    def test_del_item_from_list(self):
+        my_list = ['a','b','c']
+        my_list_without_b = ['a', 'c']
+        self.assertEqual(self.wb.del_item_from_list('b', my_list), my_list_without_b)
+
+    def test_del_piece_from_word(self):
+        word = 'batata'
+        without_ba = 'tata'
+        self.assertEqual(without_ba, self.wb.del_piece_from_word('ba', word))
+
 
 if __name__ == '__main__':
     unittest.main()
--- word_builder.py
+++ word_builder.py
@@ -2,42 +2,65 @@
 
 class WordBuilder():
     def build(self, word, word_pieces):
-
+        my_word = word
+        ## make a backup, because since lists are mutable, python passes them as references
+        my_word_pieces = list(word_pieces)
 
         ## just to count the number of letters in words pieces,
-        ## and raise exception is there is not enough letter to build
+        ## and raise exception is there is not enough letters to build
         ## the word.
         all_letters_in_word_pieces = ''
-        for piece in word_pieces:
+        for piece in my_word_pieces:
             all_letters_in_word_pieces += piece
-        if len(all_letters_in_word_pieces) < len(word):
+        if len(all_letters_in_word_pieces) < len(my_word):
             raise RuntimeError("Not enough letters in word pieces!")
-
 
         ## if there is only one piece, return that piece.
         ## It should match the word
-        if len(word_pieces) == 1:
+        if len(my_word_pieces) == 1:
             return [0]
 
+        ## code that does something is here.
+        pieces_in_order = self.get_pieces_in_order(my_word_pieces, my_word)
+        idx_in_order = []
+        for piece in pieces_in_order:
+            idx_in_order.append(word_pieces.index(piece))
+        if len(pieces_in_order) == 0:
+            print("Impossible match!")
+        return idx_in_order
 
-        ## code that does something is here,
-        ## only works with pieces of one or two letters
-        idx_list = []
-        letters = list(word)
-        skip_next_letter = False
+    # recursive function
+    def get_pieces_in_order(self, word_pieces, word):
+        pieces_in_order = []
+        next_piece_candidates = self.find_starting_pieces_candidates(word_pieces, word)
+        for next_piece_candidate in next_piece_candidates:
+            pieces_in_order = []
+            pieces_in_order.append(next_piece_candidate)
+            pieces_in_order += self.get_pieces_in_order(self.del_item_from_list(next_piece_candidate, word_pieces),
+                                                       self.del_piece_from_word(next_piece_candidate, word))
+            if self.word_pieces_match_word(pieces_in_order, word):
+                break
+        return pieces_in_order
 
-        for idx, letter in enumerate(letters):
-            if skip_next_letter == True:
-                skip_next_letter = False
-                continue
-            try:
-                two_letters = letter + letters[idx+1]
-                if two_letters in word_pieces:
-                    idx_list.append(word_pieces.index(two_letters))
-                    skip_next_letter = True
-                elif letter in word_pieces:
-                    idx_list.append(word_pieces.index(letter))
-            except IndexError:
-                if letter in word_pieces:
-                    idx_list.append(word_pieces.index(letter))
-        return idx_list
+    def find_starting_pieces_candidates(self, word_pieces, word):
+        starting_pieces = []
+        for piece in word_pieces:
+            if word.startswith(piece):
+                starting_pieces.append(piece)
+        return starting_pieces
+
+    def word_pieces_match_word(self, word_pieces, word):
+        word_from_pieces = ''
+        for piece in word_pieces:
+            word_from_pieces += piece
+        return word_from_pieces == word
+
+    def del_piece_from_word(self, piece, word):
+        if word.startswith(piece):
+            word = word[len(piece):]
+        return word
+
+    def del_item_from_list(self, element, pylist):
+        pylist_without_element = pylist
+        del pylist_without_element[pylist_without_element.index(element)]
+        return pylist_without_element
